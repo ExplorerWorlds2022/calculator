ea89dff867f2597d8b890ca59fa5471ccd03163a
ExplorerWorlds2022 <pavel.gorinov02@gmail.com>
2024-11-26 09:10:12 +0300

    10 with comments

--- calculator.py
+++ calculator.py
@@ -36,7 +36,7 @@ to_num = {
     "семьсот": 700,
     "восемьсот": 800,
     "девятьсот": 900,
-    "тысяча": 1000
+    "тысяча": 1000,
 }
 
 to_words = {value: key for key, value in to_num.items()}
@@ -49,6 +49,17 @@ operations = {
 
 
 def main():
+    """Запрашивает у пользователя математические выражения, записанные словами.
+    Программа запрашивает математические выражения, пока пользователь на введёт "выход"
+
+    Raises:
+        ValueError: 1) число меньше нуля или больше ста
+            2) слово не является ни числом, ни математической операцией
+        TypeError: возникает при неправильном чередовании чисел и операций
+
+    Side effects:
+        выводит на экран результат вычислений в словесной форме
+    """
     print(
         """Добро пожаловать в калькулятор! 
 Введите <число> <операция> <число> в формате "двадцать пять плюс тринадцать один". 
@@ -57,7 +68,7 @@ def main():
     )
 
     while True:
-        string = input("Выражение: ")
+        string = input("Выражение: ").lower().strip()
         if string == "выход":
             break
 
@@ -65,9 +76,30 @@ def main():
             print(translate_number(eval(translate_words(string))))
         except ValueError as err1:
             print(err1)
+        except TypeError:
+            print("Неправильная запись операций!")
+        except:
+            pass
 
 
 def translate_words(string):
+    """Переводит выражение, записанное словами, в математическую форму
+
+    Args:
+        string (str): математическое выражение, записанное словами
+
+    Returns:
+        str: выражение, записанное в виде цифр и знаков
+
+    Examples:
+        translate_words("двадцать пять плюс тринадцать") -> "тридцать восемь"
+        translate_words("пять плюс два умножить три минус один") -> "десять"
+
+    Raises:
+        ValueError: 1) число меньше нуля или больше ста
+            2) слово не является ни числом, ни математической операцией
+        TypeError: возникает при неправильном чередовании чисел и операций
+    """
     words = string.split()
     expression = []
     num = None
@@ -75,25 +107,38 @@ def translate_words(string):
     for word in words:
         if word in to_num:
             num = to_num[word] if num == None else num + to_num[word]
-        elif word in operations:
-            if 0 <= num <= 100:
-                expression.extend([str(num), operations[word]])
-                num = None
-            else:
-                raise ValueError('Все числа должны быть от 0 до 100!')
+        elif word in operations and check_num(num):
+            expression.extend([str(num), operations[word]])
+            num = None
         else:
             raise ValueError(f'Незнакомое слово "{word}"!')
 
-    if num != None:
-        if 0 <= num <= 100:
-            expression.append(str(num))
-        else:
-            raise ValueError('Все числа должны быть от 0 до 100!')
+    if check_num(num):
+        expression.append(str(num))
 
-    return ' '.join(expression)
+    return " ".join(expression)
+
+
+def check_num(num):
+    if (0 <= num <= 100) == False:
+        raise ValueError("Все числа должны быть от 0 до 100!")
+    return True
 
 
 def translate_number(num):
+    """Переводит число в словесную форму
+
+    Args:
+        num (int): число
+
+    Returns:
+        str: словесную форму числа
+
+    Examples:
+        translate_number(16) -> шестнадцать
+        translate_number(-105) -> минус сто пять
+        translate_number(2) -> два
+    """
     result = []
     if num < 0:
         result.append("минус")
